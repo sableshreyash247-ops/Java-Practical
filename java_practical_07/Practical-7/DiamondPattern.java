be8863c00ab3f42534971856709e85578f6ff978
sableshreyash247-ops <sableshreyash247@gmail.com>
2026-04-23 22:08:12 +0530

    Add files via upload

---
+++ java_practical_07/Practical-7/DiamondPattern.java
@@ -0,0 +1,89 @@
+class DiamondPattern {
+
+    public static void main(String[] args) {
+
+        int n = 9;
+
+        if (n % 2 == 0) {
+            System.out.println("Invalid size");
+            return;
+        }
+
+        System.out.println("LOGIC 1:\n");
+        logic1(n);
+
+        System.out.println("\nLOGIC 2:\n");
+        logic2(n);
+
+        System.out.println("\nLOGIC 3:\n");
+        logic3(n);
+    }
+
+    
+    static void logic1(int n) {
+
+        String s = "*";
+        String sp = " ";
+
+        int mid = n / 2;
+
+        for (int i = 0; i < n; i++) {
+
+            int outer = Math.abs(mid - i);
+            int inner = n - 2 * outer - 2;
+
+            if (inner < 0) {
+                System.out.println(sp.repeat(outer) + s);
+            } else {
+                System.out.println(sp.repeat(outer) + s + sp.repeat(inner) + s);
+            }
+        }
+    }
+
+    
+    static void logic2(int n) {
+
+        String s = "*";
+        String sp = "          "; 
+        int mid = n / 2;
+
+        for (int i = 0; i < n; i++) {
+
+            int outer = Math.abs(mid - i);
+            int inner = n - 2 * outer - 2;
+
+            if (inner < 0) {
+                System.out.println(sp.substring(0, outer) + s);
+            } else {
+                System.out.println(
+                        sp.substring(0, outer) + s +
+                        sp.substring(0, inner) + s
+                );
+            }
+        }
+    }
+
+    
+    static void logic3(int n) {
+
+        String s = "*";
+        String sp = "X";
+
+        int mid = n / 2;
+
+        for (int i = 0; i < n; i++) {
+
+            int outer = Math.abs(mid - i);
+            int inner = n - 2 * outer - 2;
+
+            String outSpace = sp.repeat(outer).replace("X", " ");
+
+            if (inner < 0) {
+                System.out.println(outSpace + s);
+            } else {
+                String inSpace = sp.repeat(inner).replace("X", " ");
+                System.out.println(outSpace + s + inSpace + s);
+            }
+        }
+    }
+}
